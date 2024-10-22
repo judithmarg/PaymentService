c6f8ad70c7d07a4889e05e0931afc5e980037890
judithMarg <judithmargaritapacollalli@gmail.com>
2024-10-22 03:14:21 -0400

    add doc

---
+++ src/main/java/com/example/PaymentService/PaymentApi.java
@@ -0,0 +1,39 @@
+package com.example.PaymentService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+
+import java.util.List;
+
+public interface PaymentApi {
+    @Tag( name = "Payment", description = "Obtain the info for payment service")
+    @Operation(
+            summary = "Payment result",
+            description = "Validate the info for payment"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "Successful"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "${api.responseCodes.internalServer.description}",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            schema = @Schema(implementation = ErrorResponse.class))
+                            }
+                    ),
+            }
+    )
+    ResponseEntity<String> processPayment( PaymentRequest paymentRequest );
+
+}
+
+
--- src/main/java/com/example/PaymentService/PaymentController.java
+++ src/main/java/com/example/PaymentService/PaymentController.java
@@ -1,6 +1,7 @@
 package com.example.PaymentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/payment")
-public class PaymentController {
+public class PaymentController implements PaymentApi{
 
     @Autowired
     private PaymentService paymentService;
 
     @PostMapping("/process")
-    public String processPayment(@RequestBody PaymentRequest paymentRequest) {
-        return paymentService.processPayment(paymentRequest);
+    public ResponseEntity<String> processPayment(@RequestBody PaymentRequest paymentRequest) {
+        String res = paymentService.processPayment(paymentRequest);
+        return ResponseEntity.ok(res);
     }
 }
